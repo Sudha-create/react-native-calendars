da6dcd3e61e1578ded51b256123d23164885ca12
Akhilesh Mourya <akhileshmourya@nattyhatty.com>
2023-06-30 15:25:44 +0530

    Changed in timeline card style

--- src/timeline/EventBlock.tsx
+++ src/timeline/EventBlock.tsx
@@ -1,4 +1,4 @@
-import XDate from 'xdate';
+// import XDate from 'xdate';
 import React, {useCallback, useMemo} from 'react';
 import {View, Text, TextStyle, TouchableOpacity, ViewStyle} from 'react-native';
 
@@ -17,6 +17,12 @@ export interface PackedEvent extends Event {
   top: number;
   width: number;
   height: number;
+  borderRadius: number;
+  borderTopWidth: number;
+  borderTopColor: string;
+  titleStyle: ViewStyle | TextStyle;
+  summaryStyle: ViewStyle | TextStyle;
+  duration: string;
 }
 
 export interface EventBlockProps {
@@ -32,19 +38,22 @@ const TEXT_LINE_HEIGHT = 17;
 const EVENT_DEFAULT_COLOR = '#add8e6';
 
 const EventBlock = (props: EventBlockProps) => {
-  const {index, event, renderEvent, onPress, format24h, styles} = props;
+  const {index, event, renderEvent, onPress, styles} = props;
 
   // Fixing the number of lines for the event title makes this calculation easier.
   // However it would make sense to overflow the title to a new line if needed
   const numberOfLines = Math.floor(event.height / TEXT_LINE_HEIGHT);
-  const formatTime = format24h ? 'HH:mm' : 'hh:mm A';
+  // const formatTime = format24h ? 'HH:mm' : 'hh:mm A'; --- format24h import from pros
   const eventStyle = useMemo(() => {
     return {
       left: event.left,
       height: event.height,
       width: event.width,
       top: event.top,
-      backgroundColor: event.color ? event.color : EVENT_DEFAULT_COLOR
+      backgroundColor: event.color ? event.color : EVENT_DEFAULT_COLOR,
+      borderRadius: 4,
+      borderTopWidth: 6,
+      borderTopColor: event.borderTopColor ? event.borderTopColor : EVENT_DEFAULT_COLOR
     };
   }, [event]);
 
@@ -58,19 +67,24 @@ const EventBlock = (props: EventBlockProps) => {
         renderEvent(event)
       ) : (
         <View>
-          <Text numberOfLines={1} style={styles.eventTitle}>
+          <Text numberOfLines={1} style={event.titleStyle}>
             {event.title || 'Event'}
           </Text>
           {numberOfLines > 1 ? (
-            <Text numberOfLines={numberOfLines - 1} style={[styles.eventSummary]}>
+            <Text numberOfLines={numberOfLines - 1} style={[event.summaryStyle]}>
               {event.summary || ' '}
             </Text>
           ) : null}
-          {numberOfLines > 2 ? (
+          {numberOfLines > 1 ? (
+            <Text numberOfLines={numberOfLines - 1} style={[event.summaryStyle]}>
+              {event?.duration || ' '}
+            </Text>
+          ) : null}
+          {/* {numberOfLines > 2 ? (
             <Text style={styles.eventTimes} numberOfLines={1}>
               {new XDate(event.start).toString(formatTime)} - {new XDate(event.end).toString(formatTime)}
             </Text>
-          ) : null}
+          ) : null} */}
         </View>
       )}
     </TouchableOpacity>
